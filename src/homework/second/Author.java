f4b068603901199dd6e17ec4edd0d08b2e64447c
Nataliia Nikitina <khmelovska.ng@gmail.com>
2019-02-27 15:40:44 +0200

    second homework updated second time

--- src/homework/second/Author.java
+++ src/homework/second/Author.java
@@ -2,11 +2,18 @@ package homework.second;
 
 import java.util.ArrayList;
 import java.util.List;
+
 //2.4
 public final class Author {
     final private int pages;
     final private String name;
     final private List<Story> stories;
+
+    public Author(int pages, String name, ArrayList<Story> stories) {
+        this.pages = pages;
+        this.name = name;
+        this.stories = (stories == null) ? null : new ArrayList<Story>(stories);
+    }
 
     public int getPages() {
         return pages;
@@ -17,16 +24,7 @@ public final class Author {
     }
 
     public List<Story> getStories() {
-        return new ArrayList<Story>(stories);
-    }
-
-    public Author(int pages, String name, ArrayList<Story> stories) {
-        this.pages = pages;
-        this.name = name;
-        this.stories = new ArrayList<Story>(stories);
-        if (stories==null){
-            ArrayList<Story> listOfStory = new ArrayList<Story>();
-        }
+        return (stories==null)?null: new ArrayList<Story>(stories);
     }
 
     public final static class Story {
